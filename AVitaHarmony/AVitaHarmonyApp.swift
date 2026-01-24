6588993c6af871b10b40044826e6eff0b5d46b74
ludik <bakhanetssimon@gmail.com>
2026-01-24 19:39:24 +0300

    Initial Commit

---
+++ AVitaHarmony/AVitaHarmonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVitaHarmonyApp.swift
+//  AVitaHarmony
+//
+//  Created by Simon Bakhanets on 24.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct AVitaHarmonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
